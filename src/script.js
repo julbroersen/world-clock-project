122748b76490f254091027bbd0904e4f8c1a3731
julbroersen <julianabroersen2@gmail.com>
2025-02-06 15:01:19 +0100

    added update city function

--- src/script.js
+++ src/script.js
@@ -10,6 +10,37 @@ function updateTime() {
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
     "HH:mm:ss [<small>]A[</small>]"
   );
+
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
+
+  torontoDateElement.innerHTML = torontoTime.format("dddd, MMMM Do, YYYY");
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="current-time">
+          <div>
+            <h2 class="city">${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCity);
